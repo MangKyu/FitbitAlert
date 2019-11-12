7c88b7ea95512a0d88703f5b909b902e9787cbe1
MangKyu <enendekekd12>
2019-11-12 14:23:03 +0900

    create Constant Module

---
+++ app/src/main/java/com/fitbitalert/Utils/Consts/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.fitbitalert.Utils.Consts;
+
+public final class ApiConstants {
+
+    public static final String SERVER_URL = "http://117.16.136.156:8080/";
+
+}
---
+++ app/src/main/java/com/fitbitalert/Utils/Consts/AppConstants.java
@@ -0,0 +1,7 @@
+package com.fitbitalert.Utils.Consts;
+
+public final class AppConstants {
+
+    public static final String APP_NAME = "Fitbit Alert";
+
+}
