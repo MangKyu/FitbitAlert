de3e882ef9dc32fedd0f94996abc1271d1ede2ce
MangKyu <enendekekd12>
2019-11-15 17:50:57 +0900

    Add provideRxBleClient on AppModule

--- app/src/main/java/com/fitbitalert/DI/Module/AppModule.java
+++ app/src/main/java/com/fitbitalert/DI/Module/AppModule.java
@@ -3,11 +3,18 @@ package com.fitbitalert.DI.Module;
 import android.content.Context;
 
 import com.fitbitalert.Common.BaseApplication;
+import com.fitbitalert.Utils.Logs.Dlog;
+import com.polidea.rxandroidble2.LogConstants;
+import com.polidea.rxandroidble2.LogOptions;
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.exceptions.BleException;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 
 @Module
 public class AppModule {
@@ -18,4 +25,26 @@ public class AppModule {
         return baseApplication;
     }
 
+    @Provides
+    RxBleClient provideRxBleClient(BaseApplication baseApplication){
+        RxBleClient rxBleClient = RxBleClient.create(baseApplication);
+        RxBleClient.updateLogOptions(new LogOptions.Builder()
+                .setLogLevel(LogConstants.INFO)
+                .setMacAddressLogSetting(LogConstants.MAC_ADDRESS_FULL)
+                .setUuidsLogSetting(LogConstants.UUIDS_FULL)
+                .setShouldLogAttributeValues(true)
+                .build()
+        );
+
+        RxJavaPlugins.setErrorHandler(throwable -> {
+            if (throwable instanceof UndeliverableException && throwable.getCause() instanceof BleException) {
+                Dlog.v("Suppressed UndeliverableException: " + throwable.toString());
+                return; // ignore BleExceptions as they were surely delivered at least once
+            }
+            // add other custom handlers if needed
+            throw new RuntimeException("Unexpected Throwable in RxJavaPlugins error handler", throwable);
+        });
+
+        return rxBleClient;
+    }
 }
